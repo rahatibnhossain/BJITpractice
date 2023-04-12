10a6cee795745c538dfb507d0abe815a241a8e85
Rahat Ibna Hossain <rahat.hossain@bjitacademy.com>
2023-04-12 15:56:56 +0600

    Done 12 April

--- src/co/bjit/task/StudentManagement.java
+++ src/co/bjit/task/StudentManagement.java
@@ -6,19 +6,25 @@ import java.util.Scanner;
 
 public class StudentManagement {
     Scanner sc = new Scanner(System.in);
-    static List<Person> StudentTeacherList = new ArrayList<Person>();
+    static List<Person> StudentTeacherList = new ArrayList<>();
+    static List<Teacher> TeacherList = new ArrayList<>();
+    static List<Student> StudentList = new ArrayList<>();
+    static List<Staff> StaffList = new ArrayList<>();
 
 
     public static void main(String[] args) {
 
 
+
         StudentTeacherList.add(new Teacher(1, "Nira Khan", 31, "Math"));
+        TeacherList.add(new Teacher(1, "Nira Khan", 31, "Math"));
         StudentTeacherList.add(new Student(2, "Hira Khan", 16, 10));
+        StudentList.add(new Student(2, "Hira Khan", 16, 10));
         StudentTeacherList.add(new Staff(3, "Hima Khan", 18, "Pion"));
+        StaffList.add(new Staff(3, "Hima Khan", 18, "Pion"));
 
         StudentManagement sm = new StudentManagement();
         sm.start();
-
 
     }
 
@@ -66,13 +72,130 @@ public class StudentManagement {
         }
     }
 
-    public void selectMember(){
+    public void selectMember() {
         System.out.println();
         System.out.println("Enter the ID of the Person You Want to Select");
         int id = sc.nextInt();
         sc.nextLine();
+        for (Person p : StudentTeacherList) {
+            if (p.id == id) {
+
+                p.showDetails();
+                System.out.println();
+                isAdult(p);
+
+                if (p.type == "Staff") {
+                    for (Staff sf : StaffList) {
+                        if (sf.id == id) {
+                            for (; ; ) {
+                                System.out.println();
+                                System.out.println("1 Set Salary");
+                                System.out.println("2 View Salary");
+                                System.out.println("3 Years of Experience");
+                                System.out.println("0 Go To Main Menu");
+                                System.out.print("Enter the option: ");
+
+                                int x = sc.nextInt();
+                                sc.nextLine();
+                                boolean shouldBreak = false;
+
+                                switch (x) {
+                                    case 1:
+                                        sf.setSalary();
+                                        break;
+                                    case 2:
+                                        sf.showSalary();
+                                        break;
+                                    case 3:
+                                        sf.yearsOfExperience();
+                                        break;
+
+                                    case 0:
+                                        shouldBreak = true;
+
+                                }
+                                if (shouldBreak) break;
+
+                            }
+
+
+                        }
+                    }
+                }
+                if (p.type == "Student") {
+                    for (Student sf : StudentList) {
+                        if (sf.id == id) {
+                            for (; ; ) {
+                                System.out.println();
+                                System.out.println("1 Years of Experience");
+                                System.out.println("0 Go To Main Menu");
+                                System.out.print("Enter the option: ");
+
+                                int x = sc.nextInt();
+                                sc.nextLine();
+                                boolean shouldBreak = false;
+
+                                switch (x) {
+
+                                    case 1:
+                                        sf.yearsOfExperience();
+                                        break;
+
+                                    case 0:
+                                        shouldBreak = true;
+
+                                }
+                                if (shouldBreak) break;
+
+                            }
+
+
+                        }
+                    }
+                }
+                if (p.type == "Teacher") {
+                    for (Teacher sf : TeacherList) {
+                        if (sf.id == id) {
+                            for (; ; ) {
+                                System.out.println();
+                                System.out.println("1 Set Salary");
+                                System.out.println("2 View Salary");
+                                System.out.println("3 Years of Experience");
+                                System.out.println("0 Go To Main Menu");
+                                System.out.print("Enter the option: ");
+
+                                int x = sc.nextInt();
+                                sc.nextLine();
+                                boolean shouldBreak = false;
+
+                                switch (x) {
+                                    case 1:
+                                        sf.setSalary();
+                                        break;
+                                    case 2:
+                                        sf.showSalary();
+                                        break;
+                                    case 3:
+                                        sf.yearsOfExperience();
+                                        break;
+
+                                    case 0:
+                                        shouldBreak = true;
+
+                                }
+                                if (shouldBreak) break;
+
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
 
     }
+
     public void addStaff() {
         System.out.println();
         System.out.println("Enter the details of the Staff");
@@ -100,8 +223,9 @@ public class StudentManagement {
         System.out.println("List of all members");
         for (Person p : StudentTeacherList) {
             p.showDetails();
-        }
 
+
+        }
     }
 
 
@@ -148,29 +272,41 @@ public class StudentManagement {
         showMembers();
     }
 
+    //using Child classes Object as an input where object of
     public void isAdult(Person p) {
         if (p.age >= 18) {
-            System.out.println("Yes, you are adult");
+            System.out.println("Yes, He is an adult");
         } else {
-            System.out.println("Sorry, you are not adult");
+            System.out.println("Sorry, He is not an adult");
         }
     }
 
 
 }
+interface Experience {
+    void yearsOfExperience();
+}
 
-class Person {
+class Person implements Experience {
     public String name;
     public int id;
     public int age;
+    public int experienceYear = 0;
 
     public String type;
 
+
+    @Override
+    public void yearsOfExperience() {
+        System.out.println("This person have experience of " + experienceYear + " years");
+    }
     public Person(int id, String name, int age) {
         this.id = id;
         this.name = name;
         this.age = age;
     }
+
+
 
     public void showDetails() {
         System.out.println("ID :" + this.id + " Name : " + this.name + " Age :" + this.age + " Type : " + this.type);
@@ -182,6 +318,7 @@ class Person {
 
 interface salaryFunctions {
 
+    // Declaring Properties in interface
     boolean isEmployee = true;
 
     void setSalary();
@@ -190,8 +327,19 @@ interface salaryFunctions {
 
 }
 
-class Student extends Person {
-    int classNumber;
+
+
+//Over Writing the interfaces method
+class Student extends Person implements Experience {
+    public int classNumber;
+
+    @Override
+    public void yearsOfExperience() {
+//        super.yearsOfExperience();
+        System.out.println();
+        System.out.println("Students does not have working Experience");
+    }
+
 
     public Student(int id, String name, int age, int classNumber) {
         super(id, name, age);
@@ -202,16 +350,24 @@ class Student extends Person {
 }
 
 class Teacher extends Person implements salaryFunctions {
-    String subject;
+    public String subject;
+    public int salary;
 
     @Override
     public void setSalary() {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Salary : ");
+        int salary = sc.nextInt();
+        sc.nextLine();
+        this.salary = salary;
+        showSalary();
     }
 
     @Override
     public void showSalary() {
-
+        System.out.println();
+        System.out.println("The salary of " + this.name + " is " + this.salary + " BDT");
+        System.out.println();
     }
 
     public Teacher(int id, String name, int age, String subject) {
@@ -225,8 +381,8 @@ class Teacher extends Person implements salaryFunctions {
 }
 
 class Staff extends Person implements salaryFunctions {
-    String post;
-    int salary;
+    public String post;
+    public int salary;
 
     public void setSalary() {
         Scanner sc = new Scanner(System.in);
@@ -251,7 +407,6 @@ class Staff extends Person implements salaryFunctions {
         this.type = "Staff";
 
     }
-
 
 }
 
